4277ded2119b1de2f78e72ae2ff48e2d60aee60e
migke-8 <miguelportelabispo@gmail.com>
2026-03-20 14:50:03 -0300

    rofi update

--- home/rofi.nix
+++ home/rofi.nix
@@ -1,13 +1,15 @@
 {
-  pkgs,
   config,
+  pkgs,
   ...
 }: {
   programs.rofi = {
     enable = true;
+    package = pkgs.rofi;
+
     extraConfig = {
       display-drun = "apps";
-      modes = "drun,run";
+      modes = "drun";
       font = "ArimoNerdFontPropo 15";
       show-icons = true;
       fixed-num-lines = true;
@@ -17,22 +19,25 @@
       kb-move-char-forward = "Shift+Right";
       kb-mode-next = "Control+Tab";
       kb-mode-previous = "Control+ISO_Left_Tab";
+      ml-row-left = "Left";
+      ml-row-right = "Right";
     };
+
     theme = let
       inherit (config.lib.formats.rasi) mkLiteral;
     in {
       "*" = {
         selected-normal-foreground = mkLiteral "rgba(253, 255, 241, 1)";
-        normal-spacing = mkLiteral "0";
+        normal-spacing = 0;
         foreground = mkLiteral "rgba(253, 255, 241, 1)";
         normal-foreground = mkLiteral "rgba(253, 255, 241, 1)";
         alternate-normal-background = mkLiteral "transparent";
         selected-urgent-foreground = mkLiteral "rgba(249, 38, 114, 1)";
         urgent-foreground = mkLiteral "rgba(249, 38, 114, 1)";
-        alternate-urgent-background = "transparent";
+        alternate-urgent-background = mkLiteral "transparent";
         active-foreground = mkLiteral "rgba(249, 38, 114, 1)";
         selected-active-foreground = mkLiteral "rgba(253, 255, 241, 1)";
-        alternate-active-background = "transparent";
+        alternate-active-background = mkLiteral "transparent";
         background = mkLiteral "transparent";
         bordercolor = mkLiteral "transparent";
         alternate-normal-foreground = mkLiteral "rgba(253, 255, 241, 1)";
@@ -50,59 +55,67 @@
         active-background = mkLiteral "transparent";
         selected-active-background = mkLiteral "rgba(249, 38, 114, 1)";
       };
-      window = {
+
+      "window" = {
         font-size = mkLiteral "4em";
         width = mkLiteral "35em";
         height = mkLiteral "35em";
         background-color = mkLiteral "rgba(39, 40, 34, 0.7)";
         border = mkLiteral "none";
-        padding = mkLiteral "0";
+        padding = 0;
       };
-      mainbox = {
-        children = mkLiteral "[inputbar, listview]";
-        padding = mkLiteral "0";
+
+      "mainbox" = {
+        children = map mkLiteral ["inputbar" "listview"];
+        padding = 0;
       };
-      inputbar = {
+
+      "inputbar" = {
         border-color = mkLiteral "rgba(253, 255, 241, 1)";
-        children = mkLiteral "[prompt, textbox-prompt-colon, entry]";
+        children = map mkLiteral ["prompt" "textbox-prompt-colon" "entry"];
         position = mkLiteral "center";
         expand = false;
         padding = mkLiteral "1em";
-        border = mkLiteral "0 solid 0 solid 0.2em solid";
+        border = mkLiteral "0px 0px 0.2em 0px"; # Cleaned up your '0 solid 0 solid 0.2em solid'
       };
-      prompt = {
+
+      "prompt, textbox-prompt-colon" = {
         font = "ArimoNerdFontPropo bold 24";
       };
-      textbox-prompt-colon = {
-        font ="ArimoNerdFontPropo bold 24";
-      };
-      entry = {
+
+      "entry" = {
         font = "ArimoNerdFontPropo 24";
       };
-      element-icon = {
+
+      "element-icon" = {
         size = mkLiteral "125px";
         horizontal-align = mkLiteral "0.5";
       };
-      element = {
+
+      "element" = {
         width = mkLiteral "200px";
         height = mkLiteral "200px";
         orientation = mkLiteral "vertical";
         padding = mkLiteral "10px 10px 13px 10px";
       };
+
       "element selected.normal" = {
         font = "ArimoNerdFontPropo Bold 15";
         background-color = mkLiteral "rgba(253, 255, 241, 0.4)";
       };
-      element-text = {
+
+      "element-text" = {
         font = mkLiteral "inherit";
         horizontal-align = mkLiteral "0.5";
         highlight = mkLiteral "bold #ae81ff";
       };
+
       "element-text selected.normal" = {
         width = mkLiteral "200px";
         font = "ArimoPropoNerdFont 16px";
       };
-      listview = {
+
+      "listview" = {
         flow = mkLiteral "horizontal";
         columns = 4;
         lines = 4;
