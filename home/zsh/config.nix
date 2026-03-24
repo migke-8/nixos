8c17eb9bb6305fcbed0fe0b45f91297d74a94ae1
migke-8 <miguelportelabispo@gmail.com>
2026-03-24 13:23:36 -0300

    update zsh

--- home/zsh/config.nix
+++ home/zsh/config.nix
@@ -22,7 +22,36 @@
     autosuggestion.enable = true;
     syntaxHighlighting.enable = true;
     autocd = true;
-    initContent = builtins.readFile ./init.sh;
+    initContent = ''
+# important !!!!!!!!!
+echo "before coding:"
+echo "- create timers"
+echo "- make a checklist"
+echo "- when learning: focus on only one thing"
+
+# source ''${./modules/dev.sh}
+
+# theme
+setopt PROMPT_SUBST
+local THEME_FILE="${./themes/theme.zsh}"
+[ -f "$THEME_FILE" ] && source $THEME_FILE || echo "theme with name \"$ZSH_THEME_NAME\" was not found\n"
+
+# auto completion
+autoload -U compinit; compinit
+_comp_options+=(globdots)
+zstyle ':completion:*' menu select
+zstyle ':completion:*' file-sort modification
+
+# history
+autoload -U up-line-or-beginning-search
+autoload -U down-line-or-beginning-search
+zle -N up-line-or-beginning-search
+zle -N down-line-or-beginning-search
+
+# Use [[ -n ]] to check if terminfo caps exist before binding
+[[ -n "''${terminfo[kcuu1]}" ]] && bindkey "''${terminfo[kcuu1]}" up-line-or-beginning-search
+[[ -n "''${terminfo[kcud1]}" ]] && bindkey "''${terminfo[kcud1]}" down-line-or-beginning-search
+'';
     shellAliases = {
       ll = "ls -l";
       ls = "ls --color";
---
+++ home/zsh/modules/dev.sh
@@ -0,0 +1,38 @@
+# dev enviroment
+autoload -U add-zsh-hook
+STATE_FILE="$HOME/.zsh_dev_state"
+
+if [[ -f "$STATE_FILE" ]]; then
+    export default_cd=$(sed -n "1p" "$STATE_FILE")
+else
+    export default_cd="$HOME"
+fi
+
+# Create a helper function to update it across sessions
+set_state() {
+    echo "$1\n$2" > "$STATE_FILE"
+    export default_cd="$1"
+    export NIX_SHELL="$2"
+}
+export default_cd="''${default_cd:-$HOME}"
+handle_nix_enviroment() {
+  if [[ -f flake.nix ]] ; then
+    if [[ -z $NIX_SHELL ]] ; then
+      echo "flake.nix found. Entering nix develop environment..."
+      echo "updating default cd enviroment..."
+      set_state "$(pwd)" "in"
+      exec nix develop -c "zsh" || echo "Failed to enter nix develop. Returning to previous shell."
+    fi
+  else
+    if [[ -n "$NIX_SHELL" ]]; then
+      rm $STATE_FILE
+    fi
+  fi
+}
+on_exit() {
+  rm $STATE_FILE
+}
+add-zsh-hook chpwd handle_nix_enviroment
+add-zsh-hook zshexit on_exit
+handle_nix_enviroment
+cd $default_cd
